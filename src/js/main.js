50ad9f1311d26113b835c97f2af4b953b1bba813
GedeonWest <yurkaronin@gmail.com>
2021-11-15 19:04:58 +0300

    Остановился на доработке стилей для анонсов учебных программ

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
+var swiper = new Swiper(".teachers-swiper", {
+  slidesPerView: 2,
+  spaceBetween: 32,
+  // loop: true,
+  // slidesPerView: 'auto',
+  grabCursor: true,
+  pagination: {
+    el: ".teachers-swiper__pagination",
+    clickable: true,
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: ".teachers-swiper__button-next",
+    prevEl: ".teachers-swiper__button-prev",
+  },
+  // centeredSlides: true,
 
+});
