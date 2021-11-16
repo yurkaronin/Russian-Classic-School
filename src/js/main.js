8f2b4ffd3cf481bb2de844e7e4b7662f48f260a0
GedeonWest <yurkaronin@gmail.com>
2021-11-16 18:35:47 +0300

    Почти закончил разметку и стилизацию

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,27 @@ var swiper = new Swiper(".teachers-swiper", {
   // centeredSlides: true,
 
 });
+
+var swiperTwo = new Swiper(".reviews-swiper", {
+  pagination: {
+    el: ".reviews-swiper__pagination",
+    type: "fraction",
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".reviews-swiper__button-next",
+    prevEl: ".reviews-swiper__button-prev",
+  },
+
+});
+
+// прилипающее меню в шапке
+window.onscroll = function showHeader() {
+  let header = document.querySelector('.header');
+
+  if (window.pageYOffset > header.offsetHeight) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+}
