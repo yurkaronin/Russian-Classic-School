70e45de1454d2b9e1581cf516835a492393d0a1d
GedeonWest <yurkaronin@gmail.com>
2021-11-18 20:35:39 +0300

    Закончил работу над проектом

--- src/js/main.js
+++ src/js/main.js
@@ -80,9 +80,9 @@ document.addEventListener('click', function (event) {
 });
 
 // подчёркивание активной ссылки
-document.addEventListener('click',function(event) {
+document.addEventListener('click', function (event) {
   if (event.target.classList.contains('navigation__link')) {
-    menuLink.forEach(function(y) {
+    menuLink.forEach(function (y) {
       y.classList.remove('navigation__link--active');
     });
     event.target.classList.add('navigation__link--active');
@@ -103,37 +103,109 @@ document.addEventListener('click', function (e) {
 
 
 // плавная анимация скрола по якорной ссылке
-// собираем все якоря; устанавливаем время анимации и количество кадров
 const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
   animationTime = 700,
   framesCount = 40;
 
 anchors.forEach(function (item) {
-  // каждому якорю присваиваем обработчик события
   item.addEventListener('click', function (e) {
-    // убираем стандартное поведение
     e.preventDefault();
 
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
     let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
 
-    // запускаем интервал, в котором
     let scroller = setInterval(function () {
-      // считаем на сколько скроллить за 1 такт
       let scrollBy = coordY / framesCount;
 
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
       if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
         window.scrollBy(0, scrollBy);
       } else {
-        // иначе добираемся до элемента и выходим из интервала
         window.scrollTo(0, coordY);
         clearInterval(scroller);
       }
-      // время интервала равняется частному от времени анимации и к-ва кадров
     }, animationTime / framesCount);
   });
 });
+
+
+// Диалоговые окна
+const btns = document.querySelectorAll('.btn');
+const modalOverlay = document.querySelector('.modal-overlay ');
+const modals = document.querySelectorAll('.modal');
+const buttonClose = document.querySelectorAll('.modal');
+
+btns.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let path = e.currentTarget.getAttribute('data-path');
+
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+      document.body.classList.toggle('custom-lock');
+    });
+
+    document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+    modalOverlay.classList.add('modal-overlay--visible');
+    document.body.classList.toggle('custom-lock');
+  });
+});
+
+modalOverlay.addEventListener('click', (e) => {
+
+  if (e.target == modalOverlay) {
+    modalOverlay.classList.remove('modal-overlay--visible');
+    document.body.classList.toggle('custom-lock');
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+      document.body.classList.toggle('custom-lock');
+    });
+  }
+
+  if (e.target.classList.contains('modal-close')) {
+    modalOverlay.classList.remove('modal-overlay--visible');
+    document.body.classList.toggle('custom-lock');
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+      document.body.classList.toggle('custom-lock');
+    });
+  }
+
+  if (e.target.classList.contains('button--close-text')) {
+    modalOverlay.classList.remove('modal-overlay--visible');
+    document.body.classList.toggle('custom-lock');
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+      document.body.classList.toggle('custom-lock');
+    });
+  }
+});
+
+// Кастомная валидация форм
+(function () {
+  window.addEventListener("click", function (event) {
+
+    if (event.target.classList.contains("js-validate")) {
+
+      const formParent = event.target.closest("form");
+
+      formParent.querySelectorAll(".custom-form__item-wrapper").forEach(function (item) {
+        console.log(item)
+        if (item.querySelector("[data-required]")) {
+          if (item.querySelector("[data-required]").value === '') {
+            console.log("не заполнен")
+            item.classList.add("js-field-error");
+          } else {
+            console.log("заполнен")
+            item.classList.remove("js-field-error");
+          }
+        }
+      });
+    }
+  })
+})();
+
+
+
+
+
+
+
 
