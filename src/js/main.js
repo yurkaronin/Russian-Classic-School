a285b4ea763d12fa9bb9b128cf26a94d50b5c5b4
GedeonWest <yurkaronin@gmail.com>
2021-11-20 15:35:46 +0300

    Добавил интерактивную карту и заменил дивы кнопок навигации в слайдере на кнопки

--- src/js/main.js
+++ src/js/main.js
@@ -188,6 +188,38 @@ for (let anchor of anchors) {
   });
 }
 
+// Интерактивная карта с гео меткой
+let center = [45.013460, 38.958488];
+
+function init() {
+
+  let map = new ymaps.Map("map", {
+    center: center, // ваши данные
+    zoom: 15
+  });
+
+  let placemarRkschool = new ymaps.Placemark([45.013460, 38.958488], {}, {
+    iconLayout: 'default#image', /* говорим что будем отображать на карте в качестве геометки  */
+    iconImageHref: '../img/map/balun.svg', /* указываем пусть к картинке на нашем сайте  */
+    iconImageSize: [56, 80], /* размеры картинки  */
+    iconImageOffset: [-19, -44] /* отступ от центра  */
+  });
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+  map.geoObjects.add(placemarRkschool);
+
+}
+
+ymaps.ready(init);
+
 
 
 
